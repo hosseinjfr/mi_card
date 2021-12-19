5317f3ade04b1e211e44eeaa08fccb5539ff7957
hossein jafarinejad <hosseinjn@outlook.com>
2021-12-19 22:31:29 +0330

    Add Column

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MainApp()
   );
 }
+class MainApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 100,
+                width: double.infinity,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+                child: Text('Container 1'),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+                child: Text('Container 1'),
+              ),
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
+
